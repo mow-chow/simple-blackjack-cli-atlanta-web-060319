c2d46bb68de99eec3be2dcbd2c6707f3f9d7cc24
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 12:10:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,9 +34,9 @@ def hit?(total)
   prompt_user
     user_input = get_user_input
     if user_input == "h"
-      card_total += deal_card
+      total += deal_card
     elsif user_input == "s"
-      card_total
+      total
     else
       invalid_command
     end
