b57ee1b4d3b75d2d70dbf21edf6d61b7ab1483a7
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 12:09:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,11 +31,21 @@ def initial_round
 end
 
 def hit?(total)
-  # code hit? here
+  prompt_user 
+    user_input = get_user_input 
+    if user_input == "h"
+      card_total += deal_card 
+    elsif user_input == "s"
+      card_total 
+    else
+      invalid_command
+    end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
+  get_user_input
 end
 
 #####################################################
