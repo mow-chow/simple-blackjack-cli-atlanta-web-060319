32bd94d880e013a2586254ff7e40656a1bdeb261
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 12:10:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,9 +55,9 @@ end
 def runner
   welcome
   sleep 0.75
-  card_total = initial_round
-  until card_total > 21
-    card_total = hit?(total)
+  total = initial_round
+  until total > 21
+    total = hit?(total)
     display_card_total(total)
   end
   end_game(total)
