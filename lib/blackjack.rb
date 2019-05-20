90f564a21da41bda10036740d5a8ec7c56cf1d9e
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 12:10:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,7 @@ end
 def initial_round
   card_one = deal_card
   card_two = deal_card
-  card_total = card_one + card_two
+  total = card_one + card_two
   display_card_total(total)
   return total
 end
