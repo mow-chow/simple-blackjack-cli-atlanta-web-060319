e8437d63522abaf555a9fb3378f6ebdaa84b26b4
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-20 12:07:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,11 @@ def end_game(total)
 end
 
 def initial_round
-  # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(total) 
+  return card_total
 end
 
 def hit?
